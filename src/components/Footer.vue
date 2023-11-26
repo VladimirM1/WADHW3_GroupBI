e69999e6be7886d5355d9ce6c5db965eff764d17
Kristjan908 <144695569+Kristjan908@users.noreply.github.com>
2023-11-26 22:43:40 +0200

    Update Footer.vue

---
+++ src/components/Footer.vue
@@ -0,0 +1,30 @@
+<template>
+  <footer>
+      <p> © Kristjan, Kirill, Vladimir </p>
+  </footer>
+</template>
+
+<script>
+export default {
+  name:'Footer'
+}
+</script>
+
+
+<style scoped>
+
+
+footer {
+  font-family: sans-serif;
+  background-color: gray;
+  color:black;
+  text-align: center;
+  font-size: 17px;
+  padding-top:5px;
+  padding-bottom:5px;
+  bottom:0;
+  margin-bottom: 10px;
+  position: fixed;
+  width: 100%;
+}
+</style>
